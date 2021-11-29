d5abed24ef0a668bcf8db04a659124f99b7c360a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-11-29 13:11:38 +0900

    Update syntax.rs

--- wiz/wiz_mir_syntax/src/syntax.rs
+++ wiz/wiz_mir_syntax/src/syntax.rs
@@ -13,12 +13,29 @@ struct Item {
     pub attrs: Vec<()>,
     pub visibility: (),
     pub kind: ItemKind,
+    pub span: Span,
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum ItemKind {
-    Struct,
-    Function,
-    Const,
-    Static,
+    Struct(VariantData),
+    Union(VariantData),
+    Function(),
+    Const(),
+    Static(),
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+struct VariantData {
+    fields: Vec<Field>
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+struct Field {
+    pub id: usize,
+    pub attrs: Vec<()>,
+    pub visibility: (),
+    pub span: Span,
+    pub identifier: String,
+    pub ty: (),
 }
